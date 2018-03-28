cf0418c3a49e5d8814b5c6b97ba3d492c6cd5a3e
Alex Porebski <porebski.alex@gmail.com>
2018-03-28 15:43:17 +0200

    added EnemyController, navmesh on terrain and assets for skeleton.

---
+++ Assets/Skeleton - Animated and Low Polygon/EnemyController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+/* Controls the Enemy AI */
+
+public class EnemyController : MonoBehaviour
+{
+
+    public float lookRadius = 10f;  // Detection range for player
+
+    public Transform target;   // Reference to the player
+    public NavMeshAgent agent; // Reference to the NavMeshAgent
+    public float distance;
+
+    // Use this for initialization
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        agent = GetComponent<NavMeshAgent>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        // Distance to the target
+        distance = Vector3.Distance(target.position, transform.position);
+
+        // If inside the lookRadius
+        if (distance <= lookRadius)
+        {
+            // Move towards the target
+            agent.SetDestination(target.position);
+
+            // If within attacking distance
+            if (distance <= agent.stoppingDistance)
+            {
+                //Attacking here
+                FaceTarget();   // Make sure to face towards the target
+            }
+        }
+    }
+
+    // Rotate to face the target
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    // Show the lookRadius in editor
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, lookRadius);
+    }
+}
