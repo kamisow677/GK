6cd7c5f5c52d1f4b11cfce29f22fd2692c4e5eae
Alex Porebski <porebski.alex@gmail.com>
2018-04-04 21:02:38 +0200

    attacking for enemy, playerStats : HP

--- Assets/Skeleton - Animated and Low Polygon/SkeletonScripts/EnemyController.cs
+++ Assets/Skeleton - Animated and Low Polygon/SkeletonScripts/EnemyController.cs
@@ -11,6 +11,7 @@ public class EnemyController : MonoBehaviour
     public float lookRadius = 10f;  // Detection range for player
     public float distance;
     public bool shouldMove;
+    public bool shouldFight;
 
     Transform target;   // Reference to the player
     NavMeshAgent agent; // Reference to the NavMeshAgent
@@ -21,6 +22,7 @@ public class EnemyController : MonoBehaviour
     // Use this for initialization
     void Start()
     {
+        shouldFight = false;
         shouldMove = false;
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         agent = GetComponent<NavMeshAgent>();
@@ -42,17 +44,19 @@ public class EnemyController : MonoBehaviour
             agent.SetDestination(target.position);          
             velocity.y = 0.9f;  
             shouldMove = true; //velocity.magnitude > 0.5f && agent.remainingDistance > agent.radius;
-
+            shouldFight = false;
             // Update animation parameters
 
             // If within attacking distance
             if (distance <= agent.stoppingDistance)
             {
+                shouldFight = true;
                 shouldMove = false;
                 FaceTarget();   // Make sure to face towards the target
             }
             // Update animation parameters
             animator.SetBool("move", shouldMove);
+            animator.SetBool("fight", shouldFight);
             animator.SetFloat("vx", velocity.x);
             animator.SetFloat("vy", velocity.y);
 
